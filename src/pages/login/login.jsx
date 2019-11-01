42905b4ff67c6e64f7a91f60daed07531a53b9f7
xiaoxu19980309 <1227003096@qq.com>
2019-11-01 16:07:07 +0800

    fixed:初始化

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,15 +1,46 @@
 import React,{Component} from "react";
-
+import Animate from 'rc-animate'
 
 class Login extends Component{
   state = {
     mobileCode: "",
-    username: "",
+    userAccount: "",
+    hasAlert: false,
+    alertText: "",
     password: "",
+    codeNumber: "",
+    captchaCodeImg: "",
+    showPwd: true,
+    loginWay: false,
   }
   render(){
     return (
-      <div>123456</div>
+      <Animate transitionName="fade">
+        <div className="login-container">
+          <Header title="密码登录" goBack={this.goBack} />
+          {this.state.loginWay?<form className="login-form">
+            <section className="input-container phone-number">
+              <input type="text" placeholder="账户密码" name="phone" maxLength="11" value={this.state.mobileCode}></input>
+              <button>获取验证码</button>
+            </section>
+          </form>:
+          <form className="login-form">
+            <section className="input-container">
+              <input type="text" placeholder="账号" value={this.state.userAccount}
+                onChange={this.handleInput.bind(this,"userAccount")}></input>
+            </section>
+            <section className="input-container">
+              {this.state.showPwd?<input type="text" placeholder="密码" value={this.state.password} onChange={this.handleInput.bind(this,"password")} />
+            :
+            <input type="password" placeholder="密码" value={this.state.password} onChange={this.handleInput.bind(this,"password")} />}
+            <div className={`button-switch ${this.state.showPwd?'change-to-text':''}`}>
+              <div className={`circle-button ${this.state.showPwd?'trans_to_right':''}`} onClick={this.handleInput.bind(this,"password")}></div>
+            </div>
+            </section>
+          </form>
+          }
+        </div>
+      </Animate>
     )
   }
 }
